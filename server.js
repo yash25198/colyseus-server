0fd7b1b0c19016b4f86182776a4c8cb7bf59b595
Yashwanth Reddy <yashwanth@renlans.org>
2022-08-02 22:51:04 +0530

    added cors origin

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import { Server } from "@colyseus/core";
 import { WebSocketTransport } from "@colyseus/ws-transport";
 import ChatRoom from "./chatroom.js";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 
 const server = new Server({
-    transport: new WebSocketTransport()
+    transport: new WebSocketTransport({
+        cors: cors({ origin: "*" }),      
+    })
 });
 server.define("yash", ChatRoom);
 server.listen(port);
